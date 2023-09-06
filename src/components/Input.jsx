c3fe9becba703899ce117b6a0cc5f2141794ccbb
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-06 10:43:22 +0200

    created Input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+class Input extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Write location" />
+      </div>
+    );
+  }
+}
+export default Input;
