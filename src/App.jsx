7431fac56fc68a718d4e53fc57ee9a41256663c8
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-06 10:59:51 +0200

    created state and pass props to Input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Input from './components/Input';
 class App extends React.Component {
+  state = {
+    location: '',
+    isLoading: false,
+    weather: {},
+    displayLocation: '',
+  };
+
+  setLocation = (e) => this.setState({ location: e.target.value });
+
   render() {
     return (
       <div className="app">
         <h1>Check Weather!</h1>
-        <Input />
+        <Input
+          location={this.state.location}
+          onChangeLocation={this.setLocation}
+        />
       </div>
     );
   }
