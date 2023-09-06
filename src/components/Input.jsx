0f42e486912819a4fe95bc8b6ec757fb8713544d
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-06 11:00:22 +0200

    used props

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,12 @@ class Input extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" placeholder="Write location" />
+        <input
+          type="text"
+          placeholder="Write location"
+          value={this.props.location}
+          onChange={this.props.onChangeLocation}
+        />
       </div>
     );
   }
