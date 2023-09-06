f1c95bc92618884030163999c7e8163c90c660a5
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-06 11:21:13 +0200

    created fetchWeather function with logic and used in lifecycles

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,50 @@ class App extends React.Component {
     displayLocation: '',
   };
 
+  fetchWeather = async () => {
+    try {
+      this.setState({
+        isLoading: true,
+      });
+      const res = await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${this.state.location}`
+      );
+      const data = await res.json();
+      console.log(data.results);
+
+      if (!data.results) throw new Error('Location not found');
+
+      const { latitude, longitude, timezone, name, country_code } =
+        data.results.at(0);
+
+      this.setState({
+        displayLocation: `${name} ${country_code}`,
+      });
+      const weatherRes = await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&timezone=${timezone}&daily=weathercode,temperature_2m_max,temperature_2m_min`
+      );
+      const weatherData = await weatherRes.json();
+      this.setState({ weather: weatherData.daily });
+    } catch (err) {
+      console.error(err);
+    } finally {
+      this.setState({ isLoading: false });
+    }
+  };
+
   setLocation = (e) => this.setState({ location: e.target.value });
+
+  componentDidMount() {
+    this.setState({ location: localStorage.getItem('location') || '' });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.location !== prevState.location) {
+      this.fetchWeather();
+
+      localStorage.setItem('location', this.state.location);
+    }
+  }
 
   render() {
     return (
