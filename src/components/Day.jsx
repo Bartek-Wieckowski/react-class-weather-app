b802b1922dcba4b5f3d31e3d910ecbed63249593
Bartek Wieckowski <bart.it.pro@gmail.com>
2023-09-06 11:59:11 +0200

    created components to display details of each day and weather list

---
+++ src/components/Day.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { getWeatherIcon, formatDay } from '../helpers';
+
+export default class Day extends Component {
+  render() {
+    const { date, max, min, code, isToday } = this.props;
+
+    return (
+      <li className="day">
+        <span>{getWeatherIcon(code)}</span>
+        <p>{isToday ? 'Today' : formatDay(date)}</p>
+        <p>
+          {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
+        </p>
+      </li>
+    );
+  }
+}
